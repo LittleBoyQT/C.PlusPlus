8be88ab951eb4d212ed6d19c8977a9b1e6a79556
Little Boy <LittleBoyQT@users.noreply.github.com>
2017-01-23 14:54:08 +0700

    Create Khong co bien trung gian.cpp

---
+++ Last semester 1/Nhap vao 2 so va hoan doi vi tri/Khong co bien trung gian.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a,b,c;
+    cout << "Input a = ";
+    cin >> a;
+    cout << "Input b = ";
+    cin >> b;
+
+    a = a + b;
+    // Bien a bay gio da thay doi thanh a + b;
+
+    b = a - b;
+    // Bien b da thay doi thanh = a (a nhap vao)
+
+    a = a - b
+
+    cout << "After changering possition..." << endl;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+}
