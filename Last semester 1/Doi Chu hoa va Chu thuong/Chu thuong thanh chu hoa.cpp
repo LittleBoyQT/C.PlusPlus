7942d901b9de4f17cba66466f318abfa41cd0688
Little Boy <LittleBoyQT@users.noreply.github.com>
2017-01-23 17:15:54 +0700

    Update Chu thuong thanh chu hoa.cpp

--- Last semester 1/Doi Chu hoa va Chu thuong/Chu thuong thanh chu hoa.cpp
+++ Last semester 1/Doi Chu hoa va Chu thuong/Chu thuong thanh chu hoa.cpp
@@ -5,9 +5,12 @@ int main ()
 {
     char c;
     cin >> c;
-
-    c = int(c) - 32;
-		// Chu hoa thanh chu thuong thi + 32;
-		
-    cout << char(c);
+	if (c >= 'a' && c <= 'z')
+	{
+    	c = int(c) - 32;
+		// Chu hoa thanh chu thuong thi + 32;		
+    	cout << char(c);
+	}
+	else
+		cout << "Nhap lai" << endl;
 }
