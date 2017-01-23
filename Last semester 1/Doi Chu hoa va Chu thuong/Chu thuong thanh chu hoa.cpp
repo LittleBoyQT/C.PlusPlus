7f300e336df404836b6a5fdf5c0a644e31663079
Little Boy <LittleBoyQT@users.noreply.github.com>
2017-01-23 15:36:39 +0700

    Create Chu thuong thanh chu hoa.cpp

---
+++ Last semester 1/Doi Chu hoa va Chu thuong/Chu thuong thanh chu hoa.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main ()
+{
+    char c;
+    cin >> c;
+
+    c = int(c) - 32;
+		// Chu hoa thanh chu thuong thi + 32;
+		
+    cout << char(c);
+}
