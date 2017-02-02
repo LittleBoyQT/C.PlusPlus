8647a4e32d887b80ec68d245bf0866ada2895fc2
Little Boy <LittleBoyQT@users.noreply.github.com>
2017-02-02 16:43:10 +0700

    Update array.cpp

--- Mid-term test/Array/array.cpp
+++ Mid-term test/Array/array.cpp
@@ -1,28 +1,48 @@
 #include <iostream>
 using namespace std;
 
-int MAX_VALUE(int a[]);
-int SUM(int a[]);
-void ORDER_BY(int a[]);
+void fillArray(int a[], int& size, int& changed_size);
+int MAX_VALUE(int a[], int changed_size);
+int SUM(int a[], int changed_size);
+void ORDER_BY_ASC(int a[], int changed_size);
 
 int main ()
 {
-    int a[4] = {-1,6,7,10};
+    int MAXIMUM = 100;
+    int changed_size;
+    int a[MAXIMUM];
+    // FILL ARRAY
+    fillArray(a, MAXIMUM, changed_size);
 
     // MAX VALUE
-    cout << MAX_VALUE(a) << endl;
+    cout << MAX_VALUE(a, changed_size) << endl;
 
     // SUM of array
-    cout << SUM(a) << endl;
+    cout << SUM(a, changed_size) << endl;
 
     // ORDER BY
-    ORDER_BY(a);
+    ORDER_BY_ASC(a, changed_size);
 
 }
-int MAX_VALUE(int a[])
+
+void fillArray (int a[], int& size, int& changed_size)
+{
+
+    cout << "How many values you want to enter? ...";
+    cin >> size;
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> a[i];
+    }
+    cout << endl << endl;
+    changed_size = size;
+}
+
+int MAX_VALUE(int a[], int changed_size)
 {
     int max_val = a[0];
-    for (int i = 1; i < 4;i++)
+    for (int i = 1; i < changed_size;i++)
     {
         if (a[i] > max_val)
         {
@@ -32,32 +52,31 @@ int MAX_VALUE(int a[])
     return max_val;
 }
 
-int SUM (int a[])
+int SUM (int a[], int changed_size)
 {
     int s = 0;
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < changed_size; i++)
     {
         s+=a[i];
     }
     return s;
 }
 
-void ORDER_BY(int a[])
+void ORDER_BY_ASC(int a[], int changed_size)
 {
-    for (int i = 0; i < 4; i++)
+    for (int before = 0; before < changed_size - 1; before++)
     {
-        for (int j = i + 1; i < 4; j++)
+        for (int after = before + 1; after < changed_size; after++)
         {
-            int temp = 0;
-            if (a[i] > a[j])
+            if (a[before] > a[after])
             {
-                temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
+                int temp = a[before];
+                a[before] = a[after];
+                a[after] = temp;
             }
         }
     }
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < changed_size; i++)
     {
         cout << a[i] << " ";
     }
