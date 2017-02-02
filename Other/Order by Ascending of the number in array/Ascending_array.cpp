fc8a016a9e90e67a01062b45b80080ff899cbb21
Little Boy <LittleBoyQT@users.noreply.github.com>
2017-02-02 17:48:13 +0700

    Update Ascending_array.cpp

--- Other/Order by Ascending of the number in array/Ascending_array.cpp
+++ Other/Order by Ascending of the number in array/Ascending_array.cpp
@@ -2,23 +2,8 @@
 
 using namespace std;
 
-int MAXIMUM = 10;
 
-void fillArray (int a[], int&, int&);
-void OutArray (int a[], int&);
-void orderbyasc (int a[], int&);
-
-
-int main()
-{
-    int a[MAXIMUM], size, size_changed;
-    cout << "This program will order numbers input by ascending." << endl;
-    fillArray (a, MAXIMUM, size_changed);
-    OutArray (a, MAXIMUM);
-    orderbyasc(a, size_changed);
-    return 0;
-}
-void fillArray (int a[], int& size, int& size_changed)
+void fillArray (int a[], int& size)
 {
     //int size_changed;
     cout << "Do you want how many numbers are? ";
@@ -29,36 +14,51 @@ void fillArray (int a[], int& size, int& size_changed)
         cin >> a[i];
     }
     cout << endl;
-    size_changed = size;
+
 }
-void OutArray (int a[], int& size)
-{
-    cout << "The numbers which you input are: ";
-    for (int i = 0; i < size; i++)
-    {
-        cout << a[i] << " ";
-    }
-    cout << endl;
-}
+//void OutArray (int a[], int& size)
+//{
+//    cout << "The numbers which you input are: ";
+//    for (int i = 0; i < size; i++)
+//    {
+//        cout << a[i] << " ";
+//    }
+//    cout << endl;
+//}
 void orderbyasc (int a[], int& size)
 {
-    for (int before = 0; before < size - 1; before++)
+    for (int before = 0; before < size; before++)
     {
+        //cout << "Vong 1; Lan " << before << endl;
         for (int after = before + 1; after < size; after++)
         {
+            //cout << "\tVong 2; Lan " << after << endl;
             if (a[before] > a[after])
             {
                 float temp = a[before];
                 a[before] = a[after];
                 a[after] = temp;
-            }
+            } // 8 6 9 2
+            cout << a[before] << endl;
+            cout << a[after] << endl;
         }
         cout << endl;
     }
     cout << "The order of numbers after ordering are: ";
     for (int i = 0; i < size; i++)
     {
+       // int temp;
         cout << a[i] << " ";
     }
     cout << endl << endl;
+}
+
+int main()
+{
+    int size = 10, a[size];
+    cout << "This program will order numbers input by ascending." << endl;
+    fillArray (a, size);
+    //OutArray (a, MAXIMUM);
+    orderbyasc(a, size);
+    return 0;
 }
